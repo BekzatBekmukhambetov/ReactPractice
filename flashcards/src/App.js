567c1c4c02637322e61525084088bfcd2711794f
Bekzat <bekmukhambetovbk@gmail.com>
2025-04-02 01:58:51 +0200

    feat(state): add step navigation and toggle visibility

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -1,25 +1,42 @@
-const messages = [
-  "Desc-1",
-  "Desc-2",
-  "Desc-3",
-];
+import {useState} from "react"
+const messages = ["Desc-1", "Desc-2", "Desc-3"];
 
-export default function App(){
-  const step = 2;
-return(
-  <div className="steps">
-    <div className="numbers">
-      <div className= {`${step >= 1 ? "active": " "}`}>1</div>
-      <div className= {`${step >= 2 ? "active": " "}`}>2</div>
-      <div className= {`${step >= 3 ? "active": " "}`}>3</div>
-    </div>
+export default function App() {
+  const [step, setStep] = useState(1)
+  const [isOpen , setIsOpen] = useState(true)
+  
 
-    <p className="message">Flashcard {step} {messages[step-1]} </p>
+  function handlePrevious() {
+    if(step > 1) setStep(step-1)
+  }
 
-    <div className="buttons">
-      <button style= {{backgroundColor: "#7950f2"}}>Prev</button>
-      <button style= {{backgroundColor: "#7950f2"}}>Next</button>
-    </div>
-  </div>
-);
+  function handleNext() {
+    if(step < 3) setStep(step+1)
+  }
+  return (
+    <>
+      <button className = "close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
+    { isOpen && (
+      <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : " "}>1</div>
+        <div className={step >= 2 ? "active" : " "}>2</div>
+        <div className={step >= 3 ? "active" : " "}>3</div>
+      </div>
+
+      <p className="message">
+        Flashcard {step} {messages[step - 1]}{" "}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2" }} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2" }} onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>)}
+    </>
+  );
 }
