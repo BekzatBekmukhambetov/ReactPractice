fe95465252be98470d026e8f258f48aa37e68066
Bekzat <bekmukhambetovbk@gmail.com>
2025-04-06 00:05:48 +0200

    Add basic travel list UI with item form, packing list, and initial data

---
+++ travel-list/src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Tea", quantity: 10, packed: true },
+  { id: 4, description: "Shoes", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>Travel list</h1>;
+}
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [amount , setAmount] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Travel form</h3>
+      <select value={amount} onChange={e => setAmount(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key = {item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return <footer className="stats">Hello</footer>;
+}
